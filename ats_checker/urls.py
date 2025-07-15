941f115233f3b4edb973da588f29a8be8de54b4a
DhruvRautela <dhruvrautela2004@gmail.com>
2025-07-15 07:07:47 +0530

    Initial commit

---
+++ ats_checker/urls.py
@@ -0,0 +1,27 @@
+"""
+URL configuration for ats_checker project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from resumechecker.views import JobDescriptionAPI,AnalyzerResumeAPI
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('resumechecker.urls')),
+    path('api/jobs/',JobDescriptionAPI.as_view()),
+    path('api/resume/',AnalyzerResumeAPI.as_view()),
+
+]
